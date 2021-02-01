b4c62a3dfad1afba8276ca24d0f22e5171934909
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-01 01:49:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,4 +67,18 @@ class Dog
     end.first
   end
   
+    def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  
 end
